4878150d2ba5be6d32e14c8659abf46b1576bf3d
btmccollum <btmcco8@gmail.com>
2018-12-19 20:51:58 -0600

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,24 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
+var newDiv = document.createElement('div');
+newDiv.innerHTML = 'Hello, DOM!';
+newDiv.style.backgroundColor = '#f9f9f9';
+newDiv.style.textAlign = 'center';
+document.body.appendChild(newDiv);
+
+var ul = document.createElement('ul');
+ 
+for (let i = 0; i < 3; i++) {
+  let li = document.createElement('li');
+  li.innerHTML = (i + 1).toString();
+  ul.appendChild(li);
+}
+ 
+newDiv.appendChild(ul);
+
+ul.style.textAlign = 'left';
+
+//remove an element from dom
+ul.removeChild(ul.querySelector('li:nth-child(2)'));
+
+ul.remove();
